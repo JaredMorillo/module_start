52cb26b958a534c4f34bd7f8fa84967b2db6ed18
JaredMorillo <jared.morillo@hotmail.com>
2026-03-06 20:19:34 -0500

    Hello World

--- hello.sql
+++ hello.sql
@@ -1,4 +1,12 @@
 -- Hello World SQL Program
 -- In order to demostrates a simple SQL table and query
 
+CREATE TABLE hello_world (
+    id INT PRIMARY KEY,
+    message VARCHAR(50)
+);
+
+INSERT INTO hello_world (id, message)
+VALUES (1, 'Hello World');
+
 SELECT * FROM hello_world;
